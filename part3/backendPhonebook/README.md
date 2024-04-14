cf7eb55439b7d6abf2814ed4685acf8b93e45c4a
arc0027 <104079189+arc0027@users.noreply.github.com>
2024-04-14 17:03:33 +0200

    3.- Backend Phonebook

---
+++ part3/backendPhonebook/README.md
@@ -0,0 +1,11 @@
+# backendPhonebook
+
+```bash
+# Install dependencies
+$ npm install
+
+# Start application
+$ npm run dev
+```
+
+http://localhost:3001/
--- part3/backendPhonebook/index.js
+++ part3/backendPhonebook/index.js
@@ -1,84 +1,123 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const Person = require('./models/persons.js');
 
+const app = express();
+require('dotenv').config();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.static('build'));
 
+app.use(morgan((tokens, req, res) => {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ');
+}));
 
-const express = require('express')
-const app = express()
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
+app.get('/info', async (request, response) => {
+    const currentDate = new Date().toLocaleString();
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const persons = await Person.find({});
+    response.send(`
+        <div>
+            <p>Phonebook has info for ${persons.length} people</p>
+        </div>
+        <div>
+            <p>${currentDate} (${timeZone})</p>
+        </div>
+    `);
+});
+
+app.get('/api/persons', async (request, response) => {
+    const persons = await Person.find({});
+    response.json(persons.map(person => person.toJSON()));
+});
+
+app.get('/api/persons/:id', async (request, response, next) => {
+    try {
+        const person = await Person.findById(request.params.id);
+        if (person) {
+            response.json(person.toJSON());
+        } else {
+            response.status(404).end();
+        }
+    } catch (error) {
+        next(error);
     }
-  ]
-  
+});
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-  })
-
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-  
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
+app.delete('/api/persons/:id', async (request, response, next) => {
+    try {
+        await Person.findByIdAndRemove(request.params.id);
+        response.status(204).end();
+    } catch (error) {
+        next(error);
     }
-  })
+});
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
+app.post('/api/persons', async (request, response, next) => {
+    const body = request.body;
+    const personName = body.name;
+    const personNumber = body.number;
 
-  app.use(express.json())
-
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (Object.keys(body).length === 0) {
+        return response.status(400).json({
+            error: 'content missing'
+        });
     }
-  
-    const note = {
-      content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
 
-const PORT = 3001
+    const person = new Person({
+        name: personName,
+        number: personNumber
+    });
+
+    try {
+        const savedPerson = await person.save();
+        const savedAndFormattedPerson = savedPerson.toJSON();
+        console.log(`added ${person.name} number ${person.number} to phonebook`);
+        response.json(savedAndFormattedPerson);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.put('/api/persons/:id', async (request, response, next) => {
+    const body = request.body;
+    const person = {
+        name: body.name,
+        number: body.number
+    };
+
+    try {
+        const updatedPerson = await Person.findByIdAndUpdate(request.params.id, person, { new: true });
+        response.json(updatedPerson.toJSON());
+    } catch (error) {
+        next(error);
+    }
+});
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
